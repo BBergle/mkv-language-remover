102040af72dd0cc08b7c3667bff4cb3bd02bc634
Benny Bergle <58752312+BBergle@users.noreply.github.com>
2023-11-11 15:46:46 -0500

    Dev (#5)
    * Added requirements.txt
    
    * Merge dev to main (#3)
    
    * Create docker-image.yml
    
    * Update docker-image.yml
    
    * Update docker-image.yml
    
    * Update docker-image.yml
    
    * Update docker-image.yml
    
    * Added requirements.txt (#2)
    
    * Added test var to convery.py
    
    * Updated TEST_ENV
    
    * added flush=true to print statement
    
    * added 2 passes
    
    * Updated second pass
    
    * Updated entry.sh
    
    * pip install orjson
    
    * revert
    
    * test convert.py
    
    * updates
    
    * Updated entry.sh
    
    * Removed whitespace
    
    * TEST_ENV update
    
    * Updated remove_languages.py
    
    * Updated print statements
    
    * update print
    
    * Updated print statements
    
    * REVERT
    
    * updated second pass
    
    * updated print
    
    * Added new line

--- entry.sh
+++ entry.sh
@@ -6,17 +6,28 @@ printf "\033[0;32mStarting Container...\033[0m\n"
 printf "\033[0;32mUpgrading pip...\033[0m\n"
 pip install --upgrade pip
 printf "\033[0;32mInstalling dependencies from requirements.txt...\033[0m\n"
-pip install -r requirements.txt
+pip install orjson
 
 # Fetch the convert option from the environment variable
 CONVERT_ENV=$(printenv CONVERT)
 # Fetch the remove languages option from the environment variable
 REMOVE_LANGUAGES_ENV=$(printenv REMOVE_LANGUAGES)
 
+# Function to execute Python script and handle real-time output
+run_python_script() {
+    local script_name=$1
+
+    # Run the Python script and directly pipe its output to the console
+    if ! python3 "$script_name"; then
+        printf "\033[0;31mPython script '$script_name' failed.\033[0m\n"
+        return 1  # Or the actual exit code, if needed
+    fi
+}
+
 # Check if CONVERT_ENV is set to 'True' (case-insensitive)
 if [[ "$CONVERT_ENV" =~ ^(True|true|TRUE)$ ]]; then
     printf "\033[1;33mINFO: Converting all m2ts and mp4 files to mkv\n\n\033[0m"
-    python3 convert.py || printf "\033[0;32mPython script failed with exit code $?\033[0m\n"
+    run_python_script convert.py
 else
     printf "\033[0;33mCONVERT environment variable is set to 'False', skipping Python script execution.\033[0m\n"
 fi
@@ -24,7 +35,7 @@ fi
 # Check if REMOVE_LANGUAGES_ENV is set to 'True' (case-insensitive)
 if [[ "$REMOVE_LANGUAGES_ENV" =~ ^(True|true|TRUE)$ ]]; then
     printf "\033[1;33mINFO: Removing Languages\n\n\033[0m"
-    python3 remove_languages.py || printf "\033[0;32mPython script failed with exit code $?\033[0m\n"
+    run_python_script remove_languages.py
 else
     printf "\033[0;33mREMOVE_LANGUAGES environment variable is set to 'False', skipping Remove_Languages.py script execution.\033[0m\n"
 fi
--- scripts/remove_languages.py
+++ scripts/remove_languages.py
@@ -1,23 +1,13 @@
-"""
-This script removes audio and subtitle tracks in specified languages from MKV files.
-It walks through a directory of movies, checks each MKV file, 
-and removes tracks based on the configured exclusions.
-"""
-# pylint: disable=redefined-outer-name
-# pylint: disable=too-many-locals
 import os
 import subprocess
 import time
-
-try:
-    import orjson
-except ImportError:
-    import json as orjson
+import orjson
 
 # Constants
 BASE_DIR = "/movies"
 EXCLUDED_LANGUAGES_ENV = os.getenv("LANGUAGES", "")
 REMOVE_COMMENTARY_ENV = os.getenv("REMOVE_COMMENTARY", "False")
+TEST_ENV = os.getenv("TEST") == "True"
 
 # Process environment variables
 excluded_languages = [lang.strip() for lang in EXCLUDED_LANGUAGES_ENV.split(",") if lang.strip()]
@@ -26,7 +16,6 @@ remove_commentary = REMOVE_COMMENTARY_ENV.lower() == "true"
 # Output settings
 print(f"Excluded languages: {', '.join(excluded_languages)}\n", flush=True)
 print(f"Remove commentary tracks: {remove_commentary}\n", flush=True)
-
 
 def get_exclusion_track_ids(data, languages, commentary):
     """
@@ -38,40 +27,17 @@ def get_exclusion_track_ids(data, languages, commentary):
     :return: Dict with audio and subtitle track IDs to exclude.
     """
     exclusion_ids = {"audio": [], "subtitles": []}
-    audio_track_languages = {}
-    subtitle_track_languages = {}
-
     for track in data.get("tracks", []):
         track_id, track_type = str(track["id"]), track["type"]
         track_language = track["properties"].get("language", "").lower()
         track_name = track["properties"].get("track_name", "").lower()
 
-
-        if track_type == "audio":
-            audio_track_languages[track_id] = track_language
-            if commentary and "commentary" in track_name:
-                exclusion_ids[track_type].append(track_id)
-            elif track_language in languages:
-                exclusion_ids[track_type].append(track_id)
-        elif track_type == "subtitles":
-            subtitle_track_languages[track_id] = track_language
-            if track_language in languages:
-                exclusion_ids[track_type].append(track_id)
-
-    for sub_id, sub_lang in subtitle_track_languages.items():
-        if sub_lang in languages and sub_lang not in audio_track_languages.values():
-            exclusion_ids["subtitles"].remove(sub_id)
-
-    unique_audio_languages = set(audio_track_languages.values())
-    if len(unique_audio_languages) == 1:
-        only_language = next(iter(unique_audio_languages))
-        if only_language in languages:
-            for track_id, language in audio_track_languages.items():
-                if language == only_language:
-                    exclusion_ids["audio"].remove(track_id)
+        if track_type == "audio" and (track_language in languages or (commentary and "commentary" in track_name)):
+            exclusion_ids[track_type].append(track_id)
+        elif track_type == "subtitles" and track_language in languages:
+            exclusion_ids[track_type].append(track_id)
 
     return exclusion_ids
-
 
 def count_movies(directory, extension=".mkv"):
     """Count the number of movies with a particular extension in a directory."""
@@ -82,22 +48,20 @@ def count_movies(directory, extension=".mkv"):
         if file.endswith(extension)
     )
 
-
 # Count the total number of movies
 total_movies = count_movies(BASE_DIR)
+
+# New structure to hold movie file paths for processing
+movies_to_process = []
+
+# First Pass: Scan and Collect Movies
+print("\033[94mFirst pass:\033[0m Scanning for movies to process...\n", flush=True)
 CURRENT_MOVIE = 0
-
-start_time = time.time()  # Start the timer
-
-print("Script started, looking for MKV files...", flush=True)
-
 for subdir, dirs, files in os.walk(BASE_DIR):
     for file in files:
         if file.endswith(".mkv"):
             CURRENT_MOVIE += 1
             filepath = os.path.join(subdir, file)
-            temp_filepath = os.path.join(subdir, "temp_" + file)
-
             print(
                 f"\033[0;32mChecking:\033[0m ({CURRENT_MOVIE} of {total_movies}): {filepath}",
                 flush=True,
@@ -126,69 +90,113 @@ for subdir, dirs, files in os.walk(BASE_DIR):
                 )
 
                 if exclusion_ids["audio"] or exclusion_ids["subtitles"]:
-                    print(f"Processing file: {filepath}", flush=True)
+                    movies_to_process.append(filepath)
 
-                    for track_id in exclusion_ids["audio"]:
-                        print(f"Removing audio track number {track_id}...", flush=True)
+            except Exception as e:
+                print(f"Error occurred for {filepath}: {e}", flush=True)
 
-                    for track_id in exclusion_ids["subtitles"]:
-                        print(
-                            f"Removing subtitle track number {track_id}...", flush=True
-                        )
+print(f"Total movies scanned: {CURRENT_MOVIE}", flush=True)
+print(f"Found {len(movies_to_process)} movies to process.", flush=True)
 
-                    mkvmerge_command = ["mkvmerge", "-o", temp_filepath]
-                    if exclusion_ids["audio"]:
-                        EXCLUDED_AUDIO = "!" + ",".join(exclusion_ids["audio"])
-                        mkvmerge_command.extend(["-a", EXCLUDED_AUDIO])
+# Skip the second pass if TEST_ENV is True
+if TEST_ENV:
+    print("\033[93mSkipping the second pass due to the test vairable being true.\033[0m", flush=True)
+else:
+    # Second Pass: Process the Collected Movies
+    print("\033[94m Second pass:\033[0m Processing movies...", flush=True)
+    CURRENT_MOVIE = 0
 
-                    if exclusion_ids["subtitles"]:
-                        EXCLUDED_SUBTITLES = "!" + ",".join(exclusion_ids["subtitles"])
-                        mkvmerge_command.extend(["-s", EXCLUDED_SUBTITLES])
+    start_time = time.time()  # Start the timer
 
-                    mkvmerge_command.append(filepath)
+    for filepath in movies_to_process:
+        CURRENT_MOVIE += 1
+        temp_filepath = os.path.join(os.path.dirname(filepath), "temp_" + os.path.basename(filepath))
 
-                    with open(os.devnull, "wb") as null:
-                        result = subprocess.run(
-                            mkvmerge_command, stdout=null, stderr=null, check=True
-                        )
+        print(
+            f"\033[0;32mProcessing:\033[0m ({CURRENT_MOVIE} of {len(movies_to_process)}): {filepath}",
+            flush=True,
+        )
 
-                    if result.returncode == 0:
-                        os.remove(filepath)
-                        os.rename(temp_filepath, filepath)
-                        print(
-                            f"Processing of {filepath} completed successfully.",
-                            flush=True,
-                        )
-                    else:
-                        print(
-                            f"Error processing {filepath}. Keeping the original file.",
-                            flush=True,
-                        )
+        try:
+            result = subprocess.run(
+                ["mkvmerge", "-J", filepath], capture_output=True, text=True, check=True
+            )
 
-            except orjson.JSONDecodeError as e:
-                print(f"Failed to decode JSON output for {filepath}: {e}", flush=True)
-            except KeyError as e:
+            if result.returncode != 0:
                 print(
-                    f"KeyError {e} when processing {filepath}, "
-                    f"this file may have an unexpected structure.",
+                    f"Error running mkvmerge for {filepath}: {result.stderr}",
                     flush=True,
-)
+                )
+                continue
 
-            except subprocess.CalledProcessError as e:
-                print(f"Subprocess execution failed for {filepath}: {e}", flush=True)
-            except OSError as e:
-                print(f"OS error occurred for {filepath}: {e}", flush=True)
-            except ValueError as e:
-                print(f"Value error: {e}", flush=True)
+            data = orjson.loads(result.stdout)
 
+            exclusion_ids = get_exclusion_track_ids(
+                data, excluded_languages, remove_commentary
+            )
 
-end_time = time.time()  # End the timer
-elapsed_time = end_time - start_time  # Calculate the elapsed time
+            if exclusion_ids["audio"] or exclusion_ids["subtitles"]:
+                print(f"Processing file: {filepath}", flush=True)
 
-elapsed_minutes = int(elapsed_time // 60)
-elapsed_seconds = int(elapsed_time % 60)
+                for track_id in exclusion_ids["audio"]:
+                    print(f"Removing audio track number {track_id}...", flush=True)
 
-print(
-    f"Finished checking all movies. Time taken: {elapsed_minutes} min and {elapsed_seconds} sec.",
-    flush=True,
+                for track_id in exclusion_ids["subtitles"]:
+                    print(
+                        f"Removing subtitle track number {track_id}...", flush=True
+                    )
+
+                mkvmerge_command = ["mkvmerge", "-o", temp_filepath]
+                if exclusion_ids["audio"]:
+                    EXCLUDED_AUDIO = "!" + ",".join(exclusion_ids["audio"])
+                    mkvmerge_command.extend(["-a", EXCLUDED_AUDIO])
+
+                if exclusion_ids["subtitles"]:
+                    EXCLUDED_SUBTITLES = "!" + ",".join(exclusion_ids["subtitles"])
+                    mkvmerge_command.extend(["-s", EXCLUDED_SUBTITLES])
+
+                mkvmerge_command.append(filepath)
+
+                with open(os.devnull, "wb") as null:
+                    result = subprocess.run(
+                        mkvmerge_command, stdout=null, stderr=null, check=True
+                    )
+
+                if result.returncode == 0:
+                    os.remove(filepath)
+                    os.rename(temp_filepath, filepath)
+                    print(
+                        f"Processing of {filepath} completed successfully.",
+                        flush=True,
+                    )
+                else:
+                    print(
+                        f"Error processing {filepath}. Keeping the original file.",
+                        flush=True,
+                    )
+
+        except orjson.JSONDecodeError as e:
+            print(f"Failed to decode JSON output for {filepath}: {e}", flush=True)
+        except KeyError as e:
+            print(
+                f"KeyError {e} when processing {filepath}, "
+                f"this file may have an unexpected structure.",
+                flush=True,
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Subprocess execution failed for {filepath}: {e}", flush=True)
+        except OSError as e:
+            print(f"OS error occurred for {filepath}: {e}", flush=True)
+        except ValueError as e:
+            print(f"Value error: {e}", flush=True)
+
+    end_time = time.time()  # End the timer
+    elapsed_time = end_time - start_time  # Calculate the elapsed time
+
+    elapsed_minutes = int(elapsed_time // 60)
+    elapsed_seconds = int(elapsed_time % 60)
+
+    print(
+        f"\033[92mFinished processing all movies.\033[0m \nTime taken: {elapsed_minutes} min and {elapsed_seconds} sec.",
+        flush=True,
 )
