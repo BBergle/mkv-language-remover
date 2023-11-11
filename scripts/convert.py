ec58952f1d8ddb1c8403fad6a41c6e8fe114b524
BBergle <bennybergle@icloud.com>
2023-11-11 14:26:59 -0500

    revert

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,3 +1,6 @@
+"""
+This script processes movie files in a specified directory, converting them to MKV format.
+"""
 # pylint: disable=redefined-outer-name
 
 import os
@@ -7,9 +10,6 @@ import subprocess
 BASE_DIR = '/movies'
 SUPPORTED_FORMATS = ['.m2ts', '.mp4']  # Add or remove formats as needed
 NEW_EXTENSION = '.mkv'
-
-# New boolean variable
-TEST_ENV = os.getenv("TEST", "") # Set to True to only count movies without processing
 
 def count_movies_to_process():
     """
@@ -28,42 +28,38 @@ def print_processing_message(filepath, current_count, total_count):
     """
     print(f"Processing movie {current_count + 1} out of {total_count}: {filepath}", flush=True)
 
+MOVIE_COUNT = 0  # Initialize movie counter
 TOTAL_MOVIES = count_movies_to_process()
 
-# Check if test is True, if so, print the count and exit
-if TEST_ENV:
-    print(f"Total movies to be processed: {TOTAL_MOVIES}", flush=True)
-else:
-    MOVIE_COUNT = 0  # Initialize movie counter
-    for path, _, file_list in os.walk(BASE_DIR):
-        for file_name in file_list:
-            if any(file_name.endswith(ext) for ext in SUPPORTED_FORMATS):
-                file_path = os.path.join(path, file_name)
-                base_file_name = os.path.splitext(file_name)[0]
-                temp_mkv_filepath = os.path.join(path, "temp_" + base_file_name + NEW_EXTENSION)
+for path, _, file_list in os.walk(BASE_DIR):
+    for file_name in file_list:
+        if any(file_name.endswith(ext) for ext in SUPPORTED_FORMATS):
+            file_path = os.path.join(path, file_name)
+            base_file_name = os.path.splitext(file_name)[0]
+            temp_mkv_filepath = os.path.join(path, "temp_" + base_file_name + NEW_EXTENSION)
 
-                mkvmerge_command = ['mkvmerge', '-o', temp_mkv_filepath, file_path]
+            mkvmerge_command = ['mkvmerge', '-o', temp_mkv_filepath, file_path]
 
-                print_processing_message(file_path, MOVIE_COUNT, TOTAL_MOVIES)
+            print_processing_message(file_path, MOVIE_COUNT, TOTAL_MOVIES)
 
-                with subprocess.Popen(mkvmerge_command, stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE, text=True) as process:
-                    for _ in process.stderr:
-                        pass
+            with subprocess.Popen(mkvmerge_command, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE, text=True) as process:
+                for _ in process.stderr:
+                    pass
 
-                    process.wait()
+                process.wait()
 
-                    if process.returncode == 0:
-                        print(f"Successfully remuxed to: {temp_mkv_filepath}", flush=True)
-                        os.remove(file_path)
-                        final_mkv_filepath = os.path.join(path, base_file_name + NEW_EXTENSION)
-                        os.rename(temp_mkv_filepath, final_mkv_filepath)
-                        print(f"Renamed {temp_mkv_filepath} to {final_mkv_filepath}", flush=True)
-                    else:
-                        error_msg = process.stderr.read()
-                        print(f"Error remuxing {file_path}. Error message: {error_msg}", flush=True)
+                if process.returncode == 0:
+                    print(f"Successfully remuxed to: {temp_mkv_filepath}", flush=True)
+                    os.remove(file_path)
+                    final_mkv_filepath = os.path.join(path, base_file_name + NEW_EXTENSION)
+                    os.rename(temp_mkv_filepath, final_mkv_filepath)
+                    print(f"Renamed {temp_mkv_filepath} to {final_mkv_filepath}", flush=True)
+                else:
+                    error_msg = process.stderr.read()
+                    print(f"Error remuxing {file_path}. Error message: {error_msg}", flush=True)
 
-                MOVIE_COUNT += 1
-                if MOVIE_COUNT >= TOTAL_MOVIES:
-                    print("All movies processed!", flush=True)
-                    break
+            MOVIE_COUNT += 1
+            if MOVIE_COUNT >= TOTAL_MOVIES:
+                print("All movies processed!", flush=True)
+                break
