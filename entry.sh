f63913c7447070868ffbc82276972ffb081a86b5
BBergle <bennybergle@icloud.com>
2023-11-11 02:28:30 -0500

    Added requirements.txt

--- entry.sh
+++ entry.sh
@@ -2,11 +2,12 @@
 
 printf "\033[0;32mStarting Container...\033[0m\n"
 
-# Update pip and install required package
+# Update pip and install required packages from requirements.txt
 printf "\033[0;32mUpgrading pip...\033[0m\n"
 pip install --upgrade pip
-printf "\033[0;32mInstalling dependencies...\033[0m\n"
-pip install orjson
+printf "\033[0;32mInstalling dependencies from requirements.txt...\033[0m\n"
+pip install -r requirements.txt
+
 # Fetch the convert option from the environment variable
 CONVERT_ENV=$(printenv CONVERT)
 # Fetch the remove languages option from the environment variable
@@ -14,21 +15,17 @@ REMOVE_LANGUAGES_ENV=$(printenv REMOVE_LANGUAGES)
 
 # Check if CONVERT_ENV is set to 'True' (case-insensitive)
 if [[ "$CONVERT_ENV" =~ ^(True|true|TRUE)$ ]]; then
-    # Run the Python script
     printf "\033[1;33mINFO: Converting all m2ts and mp4 files to mkv\n\n\033[0m"
     python3 convert.py || printf "\033[0;32mPython script failed with exit code $?\033[0m\n"
 else
-    # If CONVERT is not 'True', skip running the script and print a message
     printf "\033[0;33mCONVERT environment variable is set to 'False', skipping Python script execution.\033[0m\n"
 fi
 
- # Check if REMOVE_LANGUAGES_ENV is set to 'True' (case-insensitive)
+# Check if REMOVE_LANGUAGES_ENV is set to 'True' (case-insensitive)
 if [[ "$REMOVE_LANGUAGES_ENV" =~ ^(True|true|TRUE)$ ]]; then
-    # Run the Remove_Languages Python script
     printf "\033[1;33mINFO: Removing Languages\n\n\033[0m"
     python3 remove_languages.py || printf "\033[0;32mPython script failed with exit code $?\033[0m\n"
 else
-    # If REMOVE_LANGUAGES is not 'True', skip running the script and print a message
     printf "\033[0;33mREMOVE_LANGUAGES environment variable is set to 'False', skipping Remove_Languages.py script execution.\033[0m\n"
 fi
 
