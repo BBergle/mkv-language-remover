78d9a3ae67f23f6205682932ea28fd56d40aa226
Benny Bergle <58752312+BBergle@users.noreply.github.com>
2023-11-11 00:34:57 -0500

    Update convert.py

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,60 +1,56 @@
-"""
-This script processes movie files in a specified directory, converting them to MKV format.
-"""
-
 import os
 import subprocess
+import re
 
-# Constants
-BASE_DIR = '/movies'
-SUPPORTED_FORMATS = ['.m2ts', '.mp4']  # Add or remove formats as needed
-NEW_EXTENSION = '.mkv'
+# Set the base directory to the movies folder
+base_dir = '/movies'
+
+movie_count = 0  # Initialize movie counter
+
+# Supported video formats for conversion
+supported_formats = ['.m2ts', '.mp4']  # Add or remove formats as needed
 
 def count_movies_to_process():
-    """
-    Counts the number of movies in the BASE_DIR that are in SUPPORTED_FORMATS.
-    """
     count = 0
-    for root, _, files in os.walk(BASE_DIR):
+    for subdir, dirs, files in os.walk(base_dir):
         for file in files:
-            if any(file.endswith(extension) for extension in SUPPORTED_FORMATS):
+            if any(file.endswith(extension) for extension in supported_formats):
                 count += 1
     return count
 
-def print_processing_message(filepath, current_count, total_count):
-    """
-    Prints a processing message for the current movie being processed.
-    """
-    print(f"Processing movie {current_count + 1} out of {total_count}: {filepath}", flush=True)
+def print_processing_message(filepath, movie_count, total_movies):
+    print(f"Processing movie {movie_count + 1} out of {total_movies}: {filepath}", flush=True)
 
-movie_count = 0  # Initialize movie counter
 total_movies = count_movies_to_process()
 
-for root, _, files in os.walk(BASE_DIR):
+for subdir, dirs, files in os.walk(base_dir):
     for file in files:
-        if any(file.endswith(extension) for extension in SUPPORTED_FORMATS):
-            filepath = os.path.join(root, file)
+        if any(file.endswith(extension) for extension in supported_formats):
+            filepath = os.path.join(subdir, file)
+            new_extension = '.mkv'
             base_file_name = os.path.splitext(file)[0]
-            temp_mkv_filepath = os.path.join(root, "temp_" + base_file_name + NEW_EXTENSION)
+            temp_mkv_filepath = os.path.join(subdir, "temp_" + base_file_name + new_extension)
 
             mkvmerge_command = ['mkvmerge', '-o', temp_mkv_filepath, filepath]
 
             print_processing_message(filepath, movie_count, total_movies)
 
-            with subprocess.Popen(mkvmerge_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
-                for line in process.stderr:
-                    pass
+            process = subprocess.Popen(mkvmerge_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-                process.wait()
+            while True:
+                line = process.stderr.readline()
+                if not line:
+                    break
+            process.wait()
 
-                if process.returncode == 0:
-                    print(f"Successfully remuxed to: {temp_mkv_filepath}", flush=True)
-                    os.remove(filepath)
-                    final_mkv_filepath = os.path.join(root, base_file_name + NEW_EXTENSION)
-                    os.rename(temp_mkv_filepath, final_mkv_filepath)
-                    print(f"Renamed {temp_mkv_filepath} to {final_mkv_filepath}", flush=True)
-                else:
-                    print(f"Error remuxing {filepath}. Error message: {process.stderr.read()}", flush=True)
+            if process.returncode == 0:
+                print(f"Successfully remuxed to: {temp_mkv_filepath}", flush=True)
+                os.remove(filepath)
+                final_mkv_filepath = os.path.join(subdir, base_file_name + new_extension)
+                os.rename(temp_mkv_filepath, final_mkv_filepath)
+                print(f"Renamed {temp_mkv_filepath} to {final_mkv_filepath}", flush=True)
+            else:
+                print(f"Error remuxing {filepath}. Error message: {process.stderr.read()}", flush=True)
 
             movie_count += 1
             if movie_count >= total_movies:
